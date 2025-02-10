d8bd67546911a5856b10309e26f959c627ce05b2
Akash200325 <142168006+Akash200325@users.noreply.github.com>
2025-02-10 18:45:09 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
 
   int first_number, second_number, sum;
-  first_number=10;
-  second_number=20;
+  first_number=30;
+  second_number=40;
   sum = first_number + second_number; 
   cout << first_number << " + " <<  second_number << " = " << sum;    
-  cout<<"wordline task3 jenkins from AKASH K RAO";
+  cout<<"\nwordline task3 jenkins from AKASH K RAO\n";
 
   return 0;
 }
