8de3b5fd143384dd1d146cf340aabbee1628fb25
Akash200325 <142168006+Akash200325@users.noreply.github.com>
2025-02-10 18:00:16 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+  first_number=10;
+  second_numberr=20;
+  sum = first_number + second_number; 
+  cout << first_number << " + " <<  second_number << " = " << sum;    
+  cout<<"wordline task3 jenkins from AKASH K RAO";
+
+  return 0;
+}
