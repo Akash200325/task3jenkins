b8b59ee1690a2d6f8cd67481823722759730fe89
Akash200325 <142168006+Akash200325@users.noreply.github.com>
2025-02-10 18:33:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
 
   int first_number, second_number, sum;
   first_number=10;
-  second_numberr=20;
+  second_number=20;
   sum = first_number + second_number; 
   cout << first_number << " + " <<  second_number << " = " << sum;    
   cout<<"wordline task3 jenkins from AKASH K RAO";
